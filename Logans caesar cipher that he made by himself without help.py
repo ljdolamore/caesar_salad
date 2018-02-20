d3c50380980589b1c83ffa18f61b8c3a39c83d10
ljdolamore <32746015+ljdolamore@users.noreply.github.com>
2018-02-20 13:11:47 -0500

    Update Logans caesar cipher that he made by himself without help.py

--- Logans caesar cipher that he made by himself without help.py
+++ Logans caesar cipher that he made by himself without help.py
@@ -1,33 +1,52 @@
 def shift(letter, shift_amount):
-    unicode_value = ord(letter) + shift_amount
 
+    value=ord(letter)+shift_amount
 
-    new_letter = chr(unicode_value)
-
+    while value>126:
+        value-=95 
+                  
+    new_letter=chr(value)
     return new_letter
 
+#--------------------------------------
+
+def shift2(letter, shift_amount):
+
+    value=ord(letter)-shift_amount
+
+    while value<32:
+        value+=95
+
+    #print(value)
+
+    new_letter=chr(value)                 
+    return new_letter
+
+#--------------------------------------
+
 def encrypt(message, shift_amount):
-    result = ""
+    result=""
 
     for letter in message:
-        result += shift(letter, shift_amount)
-
-    return result 
-
-def decrypt(message, shift_amount):
-    result = ""
-    
-    for letter in message:
-        result += shift(letter, -shift_amount)
-
+        result+=shift(letter, shift_amount)
     return result
 
-secret_message = "Pangram.encryptzzxz!@#$%%^%"
-encrypted_message = encrypt(secret_message, 10)
-decrypted_message = decrypt(encrypted_message, 10)
+#--------------------------------------
 
+def decrypt(message, shift_amount):
+    result=""
+
+    for letter in message:
+        result+=shift2(letter, shift_amount)
+        #print(shift2(letter, shift_amount))
+    return result
+
+#--------------------------------------
+
+secret_message = "Casaer Salad!@#$%^&*#$%^&zzz???"
+encrypted_message = encrypt(secret_message, 4779)
+decrypted_message = decrypt(encrypted_message, 4779)
 print(secret_message)
 print(encrypted_message)
 print(decrypted_message)
-
 
